2c0f6fd9984cba6745fcdd547d77139f96da7c5e
WijayaGanda <wijayatyo49@gmail.com>
2024-06-23 22:12:30 +0700

    Auth Bukan Latihan

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
